980e792d88ce0df85094794e661b1c50531d8a2a
owermaiden <ower.maiden@cybertekschool.com>
2023-03-06 19:57:13 +0300

    jaVA BEANS

--- src/main/java/com/hackerrank/configstyles/javabased/JavaBasedConfiguration.java
+++ src/main/java/com/hackerrank/configstyles/javabased/JavaBasedConfiguration.java
@@ -1,7 +1,21 @@
 package com.hackerrank.configstyles.javabased;
 
+import com.hackerrank.configstyles.service.CallNotificationService;
+import com.hackerrank.configstyles.service.SmsNotificationService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JavaBasedConfiguration {
+
+
+    @Bean(name = "smsNotificationService")
+    public SmsNotificationService smsNotificationService(){
+        return new SmsNotificationService("SMS_SERVICE");
+    }
+
+    @Bean(name = "callNotificationService")
+    public CallNotificationService callNotificationService(){
+        return new CallNotificationService("CALL_SERVICE");
+    }
 }
