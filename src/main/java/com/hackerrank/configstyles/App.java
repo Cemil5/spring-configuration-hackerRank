d5b64cd8c6f114b52268945fe296789dfed138f9
owermaiden <ower.maiden@cybertekschool.com>
2023-03-06 21:26:55 +0300

    Done

--- src/main/java/com/hackerrank/configstyles/App.java
+++ src/main/java/com/hackerrank/configstyles/App.java
@@ -1,7 +1,9 @@
 package com.hackerrank.configstyles;
 
+import com.hackerrank.configstyles.service.EmailNotificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
